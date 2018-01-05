be5c502fdb1f77619e2c1d65b09b02debe4c38b2
manojkulkarni <mnjpkulkarni@gmail.com>
2018-01-05 18:35:44 -0500

    Initial Commit

--- AFGetRequest/ViewController.swift
+++ AFGetRequest/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
@@ -19,7 +20,46 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        fetchUrl(url: "https://cdn1.iconfinder.com/data/icons/simple-icons/4096/apple-4096-black.png")
+    }
 
+    func fetchUrl(url : String){
+        
+        let dest : DownloadRequest.DownloadFileDestination = {_,_ in            //repeatedly download in a destination
+            let docUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            let fileUrl = docUrl.appendingPathComponent("the_punisher.jpg")
+            return (fileUrl, [.removePreviousFile, .createIntermediateDirectories])
+        }
+        
+        
+        //print JSON
+        Alamofire.request(url).responseString { (response) in
+            print(response.value)
+            debugPrint(response)
+        }.responseJSON { (response) in
+            print(response.value)
+        }.authenticate(user: "bear", password: "test")
+        
+        
+        //download image
+        Alamofire.download(url, to: dest).responseData { (response) in
+            if let data = response.value {
+                let image = UIImage.init(data: data)
+                let imageView = UIImageView.init(frame: self.view.frame)
+                imageView.image = image
+                imageView.contentMode = .scaleAspectFill
+                DispatchQueue.main.async {          //update the main thread
+                    self.view.addSubview(imageView)
+                }
+            }
+        }.downloadProgress { (progress) in
+            print("\(progress.fractionCompleted)")
+        }
+        
+    }
 
 }
 
